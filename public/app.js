8147667c0dd48ae93b270e97e3fcf3e7c815350d
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2021-01-03 22:10:57 +0200

    Initial commit

---
+++ public/app.js
@@ -0,0 +1,15 @@
+"use strict";
+var anchor = document.querySelector("a");
+console.log(anchor.href);
+//const form = document.querySelector('form')!;
+var form = document.querySelector(".new-item-form");
+console.log(form.children);
+// inputs
+var type = document.querySelector("#type");
+var tofrom = document.querySelector("#tofrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
---
+++ src/app.ts
@@ -0,0 +1,19 @@
+const anchor = document.querySelector("a")!;
+
+console.log(anchor.href);
+
+//const form = document.querySelector('form')!;
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+console.log(form.children);
+
+// inputs
+const type = document.querySelector("#type") as HTMLInputElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
