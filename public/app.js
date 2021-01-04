db1d3ee92674f614feb69205ad1db57a74bb5487
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2021-01-04 15:44:58 +0200

    Add Invoice class

--- public/app.js
+++ public/app.js
@@ -1,15 +1,22 @@
-"use strict";
-var anchor = document.querySelector("a");
-console.log(anchor.href);
-//const form = document.querySelector('form')!;
-var form = document.querySelector(".new-item-form");
+import { Invoice } from "./classes/Invoice.js";
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+// invOne.client = 'yoshi';
+// invOne.amount = 50;
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach((inv) => {
+    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+});
+const form = document.querySelector(".new-item-form");
 console.log(form.children);
 // inputs
-var type = document.querySelector("#type");
-var tofrom = document.querySelector("#tofrom");
-var details = document.querySelector("#details");
-var amount = document.querySelector("#amount");
-form.addEventListener("submit", function (e) {
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
-const anchor = document.querySelector("a")!;
+import { Invoice } from "./classes/Invoice.js";
 
-console.log(anchor.href);
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 
-//const form = document.querySelector('form')!;
+// invOne.client = 'yoshi';
+// invOne.amount = 50;
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach((inv) => {
+  console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+});
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 console.log(form.children);
 
