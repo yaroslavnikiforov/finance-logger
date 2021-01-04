fb1a704f743fe6778c5083874af30bdfccffd559
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2021-01-04 19:06:59 +0200

    Render html template

---
+++ src/classes/ListTemplate.ts
@@ -0,0 +1,23 @@
+import { HasFormatter } from "../interfaces/HasFormatter";
+
+export class ListTemplate {
+  constructor(private container: HTMLUListElement) {}
+
+  render(item: HasFormatter, heading: string, pos: "start" | "end") {
+    const li = document.createElement("li");
+    const h4 = document.createElement("h4");
+    const p = document.createElement("p");
+
+    h4.innerText = heading;
+    li.append(h4);
+
+    p.innerText = item.format();
+    li.append(p);
+
+    if (pos === "start") {
+      this.container.prepend(li);
+    } else {
+      this.container.append(li);
+    }
+  }
+}
